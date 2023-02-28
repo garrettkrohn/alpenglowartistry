604f2b127297f571f32ad0f29a7907e616cb8171
Garrett Krohn <gkrohn@kipsu.edu>
2023-02-28 13:57:01 -0600

    got some work done on the mobile repsonsiveness

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -12,16 +12,43 @@ import MenuIcon from "@mui/icons-material/Menu";
 const NavBar = () => {
   const ctx = useContext(cartContext);
   const [showCart, setShowCart] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleCart = () => {
     setShowCart(!showCart);
   };
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const Menu = (
+    <>
+      <div className="mobile-menu">
+        {MenuItems.map((page) => (
+          <Link
+            key={page.name}
+            to={page.route}
+            className="mobile-menu-item"
+            onClick={toggleMenu}
+          >
+            {page.name}
+          </Link>
+        ))}
+      </div>
+      <div className="mobile-menu-background" onClick={toggleMenu}></div>
+    </>
+  );
+
   return (
     <div className="navbar">
       <div className="navbar-left">
         <div className="navbar-menu-icon">
-          <MenuIcon sx={{ color: "white", fontSize: 35 }} />
+          <MenuIcon
+            sx={{ color: "white", fontSize: 35 }}
+            onClick={toggleMenu}
+          />
+          {showMenu ? Menu : ""}
         </div>
         <div className="navbar-menu-list">
           {MenuItems.map((page) => (
