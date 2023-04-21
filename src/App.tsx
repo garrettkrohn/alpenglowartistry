0a97e86073dc6f4dd888cb2f6dc359cffa12309e
Garrett Krohn <gkrohn@kipsu.edu>
2023-04-21 12:22:58 -0500

    readded the query client

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,31 @@ import React from "react";
 import Contact from "./Components/Contact/Contact";
 import Commissions from './Components/Commissions/Commissions'
 import About from "./Components/About/About";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
+
     return (
-        // @ts-ignore
-    <CartProvider>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/originals" element={<Gallery filter="Originals" />} />
-        <Route path="/prints" element={<Gallery filter="Prints" />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/commissions" element={<Commissions />} />
-        <Route path="/portfolio" element={<Gallery filter="Portfolio" />} />
-      </Routes>
-      <Footer />
-    </CartProvider>
+      <QueryClientProvider client={queryClient}>
+              {/*
+    // @ts-ignore */}
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/originals" element={<Gallery filter="Originals" />} />
+            <Route path="/prints" element={<Gallery filter="Prints" />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/commissions" element={<Commissions />} />
+            <Route path="/portfolio" element={<Gallery filter="Portfolio" />} />
+          </Routes>
+          <Footer />
+        </CartProvider>
+      </QueryClientProvider>
   );
 }
 
