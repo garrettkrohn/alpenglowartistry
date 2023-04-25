9b34a84a735dd18df9c8c5d8375db82fa90263fb
Garrett Krohn <gkrohn@kipsu.edu>
2023-04-25 13:55:05 -0500

    added cart services class

--- src/Components/Checkout/Checkout.tsx
+++ src/Components/Checkout/Checkout.tsx
@@ -5,6 +5,7 @@ import cartContext from "../../Store/CartContext";
 import {useMutation, useQuery} from "@tanstack/react-query";
 import './Checkout.css'
 import {itemResource, image_dimensions} from './CheckoutDtos';
+import CartServices from "../../Services/CartServices";
 
 const Checkout = () => {
   const ctx = useContext(cartContext);
@@ -14,57 +15,59 @@ const Checkout = () => {
 
   const [stepper, setStepper] = useState(0);
 
-  async function createCart(key: string | null): Promise<cartResource> {
-    let url = '';
-    if (!key) {
-      url = 'https://api.chec.io/v1/carts';
-    } else {
-      url = `https://api.chec.io/v1/carts/${key}`;
-    }
-      return await fetch(url, {
-        //@ts-ignore
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Authorization': process.env.REACT_APP_COMMERCE_TEST_KEY
-        },
-        method: 'GET',
-      })
-          .then(response => response.json())
-          .then((data: cartResource) => {
-            setCartId(data.id);
-            console.log(data);
-            return data;
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            throw error;
-          });
-  }
+  const cartServices = new CartServices();
 
-  async function addItemToCart(cartId: string, itemId: string ): Promise<cartResource> {
-    const url = `https://api.chec.io/v1/carts/${cartId}`;
-    console.log(url);
-
-    return await fetch(url, {
-      //@ts-ignore
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Authorization': process.env.REACT_APP_COMMERCE_TEST_KEY
-      },
-      method: 'POST',
-      body: JSON.stringify(itemId),
-    })
-        .then(response => response.json())
-        .then((data: cartResource) => {
-          setCartId(data.id);
-          console.log(data);
-          return data;
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          throw error;
-        });
-  }
+  // async function createCart(key: string | null): Promise<cartResource> {
+  //   let url = '';
+  //   if (!key) {
+  //     url = 'https://api.chec.io/v1/carts';
+  //   } else {
+  //     url = `https://api.chec.io/v1/carts/${key}`;
+  //   }
+  //     return await fetch(url, {
+  //       //@ts-ignore
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'X-Authorization': process.env.REACT_APP_COMMERCE_TEST_KEY
+  //       },
+  //       method: 'GET',
+  //     })
+  //         .then(response => response.json())
+  //         .then((data: cartResource) => {
+  //           setCartId(data.id);
+  //           console.log(data);
+  //           return data;
+  //         })
+  //         .catch(error => {
+  //           console.error('Error:', error);
+  //           throw error;
+  //         });
+  // }
+  //
+  // async function addItemToCart(cartId: string, itemId: string ): Promise<cartResource> {
+  //   const url = `https://api.chec.io/v1/carts/${cartId}`;
+  //   console.log(url);
+  //
+  //   return await fetch(url, {
+  //     //@ts-ignore
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'X-Authorization': process.env.REACT_APP_COMMERCE_TEST_KEY
+  //     },
+  //     method: 'POST',
+  //     body: JSON.stringify(itemId),
+  //   })
+  //       .then(response => response.json())
+  //       .then((data: cartResource) => {
+  //         setCartId(data.id);
+  //         console.log(data);
+  //         return data;
+  //       })
+  //       .catch(error => {
+  //         console.error('Error:', error);
+  //         throw error;
+  //       });
+  // }
 
   const {
     isLoading: cartIsLoading,
@@ -74,43 +77,27 @@ const Checkout = () => {
   } = useQuery({
     queryKey: [`cart`],
     //@ts-ignore
-    queryFn: () => createCart(cartId),
+    queryFn: () => cartServices.createCart(cartId),
     enabled: false,
   });
 
   const { mutate: addItem, data: cartAddedData } = useMutation({
     mutationFn: (itemId: string) =>
         //@ts-ignore
-        addItemToCart(cardData.id),
+        cartServices.addItemToCart(cartData.id, 'prod_4WJvlKR2rJ5bYV'),
+        // addItemToCart(cardData.id),
     onMutate: () => console.log('adding item to cart'),
     onError: (err, variables, context) => {
       console.log(err, variables, context);
     },
     onSettled: () => {
-      console.log('item added to cart', cartAddedData);
-      refetchCreateCart();
+      console.log('item added to cart');
     },
   });
 
   useEffect(() => {
       refetchCreateCart();
   }, []);
-
-    const AddItemComponent = (props: {itemId: string}) => {
-        console.log(props.itemId);
-        addItem(props.itemId);
-        return <div>{props.itemId}</div>;
-    }
-
-    const addAllItemsToCart = () => {
-        if (cartData) {
-            //@ts-ignore
-            console.log('adding items', ctx.items);
-            ctx.items.map((item) => <AddItemComponent itemId={item.id} />)
-        } else {
-            console.log('no cart data');
-        }
-    }
 
   return (
       <div>
@@ -127,7 +114,7 @@ const Checkout = () => {
         <button onClick={() => refetchCreateCart()}>Refetch Cart</button>
         {/*
 // @ts-ignore */}
-        <button onClick={() => addAllItemsToCart()}>Add all items to cart</button>
+        <button onClick={() => addItem()}>Add all items to cart</button>
       </div>
       )
 };
