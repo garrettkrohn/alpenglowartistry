8f8d4fa6bf5f9a8d86b1078b0a492d2d061256e9
Garrett Krohn <gkrohn@kipsu.edu>
2023-06-12 09:21:37 -0500

    removed hardcode key

--- src/Services/commerce.ts
+++ src/Services/commerce.ts
@@ -1,6 +1,7 @@
 import Commerce from "@chec/commerce.js";
 
 export const commerce = new Commerce(
-  "pk_4694363ef98bae414d582f112099b59ea6d86821c27a7",
+  //@ts-ignore
+  process.env.REACT_APP_CHECK_PUBLIC_KEY,
   true
 );
